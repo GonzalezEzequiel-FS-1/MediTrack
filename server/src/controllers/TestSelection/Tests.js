2a8e392d6c6650c06c88df504fee7778f2839c93
Zeke <egonzalez@student.fullsail.edu>
2025-06-20 09:19:41 -0400

    Testing test selection based on selscted category

---
+++ server/src/controllers/TestSelection/Tests.js
@@ -0,0 +1,124 @@
+const Patient = require("../../models/Patient");
+const categories = require("../misc/categories.json");
+const test = require("../misc/tests.json");
+
+const testSelection = async (category) => {
+  let testToPerform = [];
+  if (!category) {
+    console.log("No category provided");
+    return [];
+  }
+  switch (category) {
+    case "General Fit For Duty":
+      testToPerform = [
+        test.Audiogram,
+        test.CMP,
+        test.CBC,
+        test.PFT,
+        test.RespQ,
+        test.RFT,
+        test.UA,
+      ];
+      break;
+    case "Welder Medical":
+      testToPerform = [
+        test.Audiogram,
+        test.CMP,
+        test.CBC,
+        test.XRay,
+        test.ECG,
+        test.PFT,
+        test.RespQ,
+        test.RFT,
+        test.Titmus,
+        test.UA,
+        test.Vision,
+      ];
+      break;
+    case "Paint Medical":
+      testToPerform = [
+        test.Audiogram,
+        test.CMP,
+        test.CBC,
+        test.XRay,
+        test.ECG,
+        test.PFT,
+        test.RespQ,
+        test.RFT,
+        test.UA,
+      ];
+      break;
+    case "Pest Control Medical":
+      testToPerform = [
+        test.Irrigation,
+        test.Audiogram,
+        test.CMP,
+        test.CBC,
+        test.XRay,
+        test.ECG,
+        test.PFT,
+        test.Titer,
+        test.RespQ,
+        test.RFT,
+        test.STSEv,
+        test.Titmus,
+        test.Ishihara,
+        test.UA,
+        test.Vision,
+      ];
+      break;
+    case "Respirator Medical":
+      testToPerform = [
+        test.Audiogram,
+        test.CMP,
+        test.CBC,
+        test.XRay,
+        test.ECG,
+        test.PFT,
+        test.RespQ,
+        test.RFT,
+        test.UA,
+      ];
+      break;
+    case "Horticulture":
+      testToPerform = [
+        test.Audiogram,
+        test.CMP,
+        test.CBC,
+        test.XRay,
+        test.ECG,
+        test.PFT,
+        test.Titmus,
+        test.UA,
+        test.Vision,
+      ];
+      break;
+    case "Hazmat Medical":
+      testToPerform = [test.CMP, test.CBC, test.XRay, test.ECG, test.UA];
+      break;
+    default:
+      testToPerform = [
+        test.Irrigation,
+        test.Audiogram,
+        test.CMP,
+        test.CBC,
+        test.XRay,
+        test.ECG,
+        test.PFT,
+        test.Titer,
+        test.RespQ,
+        test.RFT,
+        test.STSEv,
+        test.Titmus,
+        test.Ishihara,
+        test.UA,
+        test.Vision,
+      ];
+      break;
+  }
+  return testToPerform;
+};
+
+module.exports = {
+  testSelection,
+};
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const logger = require('../../logs/logger');
+const logger = require("../../logs/logger");
 const {
-  createPatient
-} = require("../controllers/patientController")
+  createPatient,
+  importTester,
+} = require("../controllers/patientController");
 
-router.get('/test-log', (req, res) => {
-  logger.info('Test route hit');
-  logger.warn('Test warning');
-  logger.error('Test error');
+router.get("/test-log", (req, res) => {
+  logger.info("Test route hit");
+  logger.warn("Test warning");
+  logger.error("Test error");
   res.status(200).json({ success: true, message: "Logs tested" });
 });
 
-router.post('/patient', createPatient)
+router.post("/patient", createPatient);
+router.post("/import", importTester);
 
 module.exports = router;
